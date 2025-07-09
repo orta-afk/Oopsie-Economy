8ccf039458dca0ef46136928c8b5122f59900d22
orta-afk <darkkboy0013@gmail.com>
2025-07-09 12:11:33 +0530

    finally working on the codebase

---
+++ src/game.cpp
@@ -0,0 +1,41 @@
+#include "game.hpp"
+
+Game::Game(){
+  initGame();
+}
+void Game::initGame(){
+  win.width = 960;
+  win.height = 540;
+  win.title = "shit";
+  window = new sf::RenderWindow(sf::VideoMode({win.width, win.height}), win.title, sf::Style::Titlebar | sf::Style::Close, sf::State::Windowed);
+  sf::Vector2i getDesktopSize = (sf::Vector2i)videoMode.getDesktopMode().size;
+  sf::Vector2i getWindowSize = (sf::Vector2i)window->getSize();
+  window->setPosition(sf::Vector2i(getDesktopSize.x/2 - getWindowSize.x/2, getDesktopSize.y/2 - getWindowSize.y/2));
+  window->setVerticalSyncEnabled(true);
+}
+
+void Game::updateGame(){
+  while(const std::optional<sf::Event>event = window->pollEvent()){
+    if(event->is<sf::Event::Closed>()){
+      window->close();
+    }
+    if(const auto* key = event->getIf<sf::Event::KeyPressed>()){
+      if(key->scancode == sf::Keyboard::Scancode::Escape){
+        window->close();
+      }
+    }
+  }
+}
+
+void Game::renderGame(){
+  window->clear(sf::Color::Black);
+  window->display();  
+}
+
+const bool Game::isRunning() const{
+  return window->isOpen();
+}
+
+Game::~Game(){
+  delete window;
+}
---
+++ src/game.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+struct windowData{
+  unsigned int width;
+  unsigned int height;
+  const char* title;  
+};
+
+class Game{
+public:
+  Game();
+  void initGame();
+  void updateGame();
+  void renderGame();
+  const bool isRunning() const;
+  ~Game();
+
+private:
+  void initStuff();
+
+private:
+  windowData win;
+  sf::RenderWindow* window;
+  sf::VideoMode videoMode;
+};
